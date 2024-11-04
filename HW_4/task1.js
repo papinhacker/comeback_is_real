9369f48ea7339d636fd6cf57ce70700d27dabe54
Cronos <mail@email.com>
2024-11-04 18:08:44 +1000

    add task1.js

---
+++ HW_4/task1.js
@@ -0,0 +1,16 @@
+/**
+ * Task 1.
+ * - Создайте переменную salary со значением 1000
+ * - Создайте переменную grade, которая должна получить значение "middle" если salary больше или равна 1000,
+ * и значение "junior" - если меньше
+ *
+ * @type {number}
+ */
+const salary = 1000;
+let grade;
+if (salary >= 1000) {
+    grade = "middle";
+} else {
+    grade = "junior";
+}
+
